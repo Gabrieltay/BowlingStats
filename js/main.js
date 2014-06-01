2746fa170ae3a0c63f1060f71319ee5f99629088
Gabriel Tay <gabrieltaykl@gmail.com>
2014-06-02 00:58:52 +0800

    pause

--- js/main.js
+++ js/main.js
@@ -132,8 +132,6 @@ function GetImage(id){
 		var len = results.rows.length;
 		if ( len > 0) {
 			var file = results.rows.item(0).file;
-			window.resolveLocalFileSystemURI(imageURI, PhotoExists, PhotoNoExists);
-			PhotoExists(file);
 			if ( file == "NULL"){
 				mId = id;
 				var string = '<div class="ui-block-a"><a href="#" data-role="button" data-theme="c" onclick="TakePhoto()"><img src="images/camera.png"></a></div>' + 
