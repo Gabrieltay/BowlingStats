ffadcc618aad6d403141b2627e22214d749ea38d
Gabriel Tay <gabrieltaykl@gmail.com>
2014-06-02 14:57:15 +0800

    chart test

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ var  init = function () {
 	
 	$("#date_list").on("delete", "li", RemoveGame);
 	$("#bowl_list").on("delete", "li", RemoveDate);
+	
+	draw();
 };
 
 $(document).ready(init);
@@ -255,3 +257,21 @@ function resetFields(){
 	$('#scoreinput').trigger("create");		
 	document.getElementById('dateinput').valueAsDate = new Date();
 }
+
+function draw() {
+
+		var d1 = [];
+		for (var i = 0; i < 14; i += 0.5) {
+			d1.push([i, Math.sin(i)]);
+		}
+
+		var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+		// A null signifies separate line segments
+
+		var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+
+		$.plot("#placeholder", [ d1, d2, d3 ]);
+
+
+	}
